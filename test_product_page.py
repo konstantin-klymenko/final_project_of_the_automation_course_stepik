8c470926de91fe28ccac59406931024d204dc16c
Konstantin <k.m.klymenko@gmail.com>
2022-05-30 00:07:14 +0300

    Mark tests like Xfail

--- test_product_page.py
+++ test_product_page.py
@@ -3,22 +3,6 @@ import time
 import pytest
 
 
-# Клиент может добавить продукт в корзину
-def test_guest_can_add_product_to_basket(browser):
-    # Товар Coders at Work
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    # Товар The shellcoder's handbook
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_product_to_basket()
-    page.switch_to_alert()
-    page.solve_quiz_and_get_code()
-    time.sleep(5)
-    page.get_text_if_product_added()
-    page.get_price_if_product_added()
-
-
 # Проверка наличия линки для логина
 @pytest.mark.inheritance
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -39,6 +23,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 # Негативный тест о том что пользователь не должен видеть сообщение об успешном добавлении товара в корзину
 @pytest.mark.neg
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     # Товар Coders at Work
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -62,6 +47,7 @@ def test_guest_cant_see_success_message(browser):
 
 # Негативный тест сообщение об успешном добавлении товара в корзину должно пропасть после добавления товара
 @pytest.mark.neg
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     # Товар Coders at Work
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -73,7 +59,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.message_disappeared_after_adding_product_to_basket()
 
 
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page_when_it_is_empty(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
@@ -82,8 +68,24 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.guest_cant_see_product_if_basket_empty()
 
 
+# Клиент может добавить продукт в корзину
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/\
+?promo=newYear2019",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/\
+?promo=newYear"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"{link}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.switch_to_alert()
+    page.solve_quiz_and_get_code()
+    time.sleep(2)
+    page.get_text_if_product_added()
+    page.get_price_if_product_added()
+
+
 # Клиент может добавить акционный продукт в корзину (ссылка содержит дополнение "?promo=offerN")
-@pytest.mark.new
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -96,7 +98,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
                                                                                             marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket_promo(browser, link):
     link = f"{link}"
     page = ProductPage(browser, link)
     page.open()
@@ -106,24 +108,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.get_text_if_product_added()
     page.get_price_if_product_added()
 
+
 # @pytest.mark.new
-# @pytest.mark.parametrize('link',
-#  ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-# def test_guest_can_add_product_to_basket(browser, link):
-#    link = f"{link}"
-#    page = ProductPage(browser, link)
-#    page.open()
-#    page.add_product_to_basket()
-#    page.switch_to_alert()
-#    page.solve_quiz_and_get_code()
-#    page.get_text_if_product_added()
-#    page.get_price_if_product_added()
+# class TestUserAddToBasketFromProductPage():
+#
+#     def test_user_can_add_product_to_basket(self, browser):
+#         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#         page = ProductPage(browser, link)
+#         page.open()
+#         page.add_product_to_basket()
+#         page.switch_to_alert()
+#         page.solve_quiz_and_get_code()
+#         time.sleep(5)
+#         page.get_text_if_product_added()
+#         page.get_price_if_product_added()
+#
+#     def test_user_cant_see_success_message(browser):
+#         # Товар Coders at Work
+#         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#         page = ProductPage(browser, link)
+#         page.open()
+#         page.guest_cant_see_success_message()
+
