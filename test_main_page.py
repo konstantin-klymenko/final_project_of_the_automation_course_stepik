a198ee7a6f4d21ccbbf6683d55d370f2fbb71915
Konstantin <k.m.klymenko@gmail.com>
2022-05-29 14:14:05 +0300

    Add tests in class with marc

--- test_main_page.py
+++ test_main_page.py
@@ -2,22 +2,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage  # импортируем в файл нужный класс
 import pytest
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage в теле теста
-    login_page.should_be_login_form()
 
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -27,4 +12,18 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.guest_cant_see_product_if_basket_empty()
 
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+        login_page = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage в теле теста
+        login_page.should_be_login_form()
 
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
