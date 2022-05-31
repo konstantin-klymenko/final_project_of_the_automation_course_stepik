42492256c07ee0c935de91b5d0a420a7ea1671d6
Konstantin <k.m.klymenko@gmail.com>
2022-05-31 23:33:06 +0300

    Made edits

--- test_login_page.py
+++ test_login_page.py
@@ -1,7 +1,7 @@
-import time
-
 from .pages.login_page import LoginPage
 import pytest
+import time
+
 
 def test_should_be_login_url_on_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -9,11 +9,13 @@ def test_should_be_login_url_on_login_page(browser):
     page.open()
     page.should_be_login_url()
 
+
 def test_should_be_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/da/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_form()
+
 
 def test_should_be_registration_form(browser):
     link = "http://selenium1py.pythonanywhere.com/da/accounts/login/"
@@ -21,11 +23,11 @@ def test_should_be_registration_form(browser):
     page.open()
     page.should_be_register_form()
 
-@pytest.mark.new
+
 def test_register_new_user(browser):
     link = "http://selenium1py.pythonanywhere.com/da/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
     page.register_new_user()
     time.sleep(6)
-    page.register_new_user_assert()
+    page.should_be_authorized_user()
